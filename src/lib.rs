361d827d25c8c77ecfb2c68d889b6b122a000751
sebadob <sebastiandobe@mailbox.org>
2024-08-29 13:42:09 +0200

    introduce `server` + `client` features and compile properly

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@
 
 #![doc = include_str!("../README.md")]
 
-#[cfg(not(target_arch = "wasm32"))]
+#[cfg(feature = "server")]
 pub mod pow;
-#[cfg(target_arch = "wasm32")]
+#[cfg(feature = "client")]
 pub mod wasm;
 
 pub type BitValue<'a> = (&'a [u8], usize);
