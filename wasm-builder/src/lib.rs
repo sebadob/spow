7b7967a7b49e869c6ab5dedba28492b1cb94be89
sebadob <sebastiandobe@mailbox.org>
2024-09-24 11:36:56 +0200

    wasm build cleanup

--- wasm-builder/src/lib.rs
+++ wasm-builder/src/lib.rs
@@ -18,7 +18,6 @@ impl Pow {
     ///
     /// You can call `init()` instead of `init_random()` to initialize with a chosen secret, which
     /// is necessary if multiple backends must be allowed to validate challenges.
-    #[wasm_bindgen]
     pub fn init_random() -> Result<(), String> {
         SPow::init_random().map_err(|e| e.to_string())?;
         Ok(())
@@ -27,26 +26,22 @@ impl Pow {
     /// Initialize the PoW backend with a chosen secret.
     ///
     /// This or `init_random()` must be called before creating new `Pow` instances.
-    #[wasm_bindgen]
     pub fn init(secret: String) {
         SPow::init(secret)
     }
 
     /// Create a new PoW challenge.
     pub fn build_challenge(valid_seconds: u32, difficulty: Option<u8>) -> Result<String, String> {
-        Ok(if let Some(difficulty) = difficulty {
+        if let Some(difficulty) = difficulty {
             SPow::with_difficulty(difficulty, valid_seconds)
-                .map_err(|e| e.to_string())?
-                .build_challenge()
         } else {
             SPow::new(valid_seconds)
-                .map_err(|e| e.to_string())?
-                .build_challenge()
-        })
+        }
+            .map_err(|e| e.to_string())
+            .map(|pow| pow.build_challenge())
     }
 
     /// Perform the work and generate a PoW
-    #[wasm_bindgen]
     pub fn work(challenge: &str) -> Result<String, String> {
         SPow::work(challenge).map_err(|e| e.to_string())
     }
@@ -55,7 +50,6 @@ impl Pow {
     ///
     /// It will return the challenge after successful validation, which could be used do implement
     /// re-use mechanisms or something like that.
-    #[wasm_bindgen]
     pub fn validate(pow: &str) -> Result<String, String> {
         let challenge = SPow::validate(pow).map_err(|e| e.to_string())?;
         Ok(challenge.to_string())
