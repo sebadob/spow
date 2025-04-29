d4d0e1c518023e11c162ee3a0fb0552a982d40e6
sebadob <sebastiandobe@mailbox.org>
2025-04-29 12:44:32 +0200

    update examples

--- examples/svelte-wasm/src/spow/wasm/spow-wasm_bg.wasm.d.ts
+++ examples/svelte-wasm/src/spow/wasm/spow-wasm_bg.wasm.d.ts
@@ -1,13 +1,9 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export const pow_work_wasm: (a: number, b: number) => any;
-export const __wbindgen_exn_store: (a: number) => void;
-export const __externref_table_alloc: () => number;
-export const __wbindgen_export_2: WebAssembly.Table;
-export const __wbindgen_export_3: WebAssembly.Table;
+export const pow_work_wasm: (a: number, b: number) => [number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
 export const __wbindgen_malloc: (a: number, b: number) => number;
 export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
-export const closure6_externref_shim: (a: number, b: number, c: any) => void;
-export const closure18_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
 export const __wbindgen_start: () => void;
