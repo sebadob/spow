4cae55ef2d721b5cefb71f63ce9d15201762dd36
sebadob <sebastiandobe@mailbox.org>
2025-05-01 17:44:24 +0200

    add comment about `fetch` inside solve fn

--- examples/svelte-wasm/src/lib/pow.ts
+++ examples/svelte-wasm/src/lib/pow.ts
@@ -2,6 +2,11 @@ export async function solvePow(challenge: string): Promise<string | undefined> {
     // spawn the worker in advance so it can async load the wasm already
     let worker = new Worker(new URL("powWorker.ts", import.meta.url));
 
+    // Usually, it makes a lot of sense to have the `fetch` inside this function as well.
+    // If the worker is spawned async in advance it can load the wasm while the `fetch`
+    // is retrieving the challenge from the backend. With this strategy, the solving of
+    // the PoW will have no real delay at all.
+
     return await new Promise((resolve) => {
         worker.onmessage = ev => {
             resolve(ev.data as string);
